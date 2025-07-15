129f148b7e9aef6648d1bb2031c92108b1bb2191
João Leonardo <joaole64@gmil.com>
2025-07-15 12:55:30 -0300

    add initial .env.exemple file and update page layout by removing unused components

---
+++ GEMINI.md
@@ -0,0 +1,6 @@
+# UI and Frontend
+
+- Always follow the style guidelines from pages already done
+- Use the same brand colors used in other pages
+- Always use Shadcn components if non native components are created in the project
+- Focus on creating meaningful micro interactions that improve user experie
--- src/app/(private)/controle-horarios/inicio/page.tsx
+++ src/app/(private)/controle-horarios/inicio/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { PainelPonto } from "@/components/inicio/painel-ponto";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -10,10 +9,8 @@ import {
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
 import { SidebarTrigger } from "@/components/ui/sidebar";
-import { useUserStore } from "@/stores/userStore";
 
 export default function Page() {
-  const user = useUserStore((state) => state.user);
   return (
     <div className="flex flex-col bg-white shadow-lg rounded-2xl p-2 sm:p-4 sm:px-6 lg:px-8 flex-1 min-h-[125vh] border dark:bg-[#1c1c20]">
       <header className="flex h-16 shrink-0 items-center gap-2">
@@ -32,21 +29,7 @@ export default function Page() {
       </header>
 
       {/* Conteúdo principal */}
-      <div className="px-4">
-        <div className="mb-6 space-y-1">
-          <h1 className="text-2xl font-semibold">Controle de Ponto</h1>
-          <p className="text-gray-600">
-            Bem vindo de volta, {user ? user.name : null}
-          </p>
-        </div>
-        {user ? (
-          <PainelPonto userId={user.id} />
-        ) : (
-          <div className="text-center text-gray-500">
-            <p>Carregando...</p>
-          </div>
-        )}
-      </div>
+      <div className="px-4"></div>
     </div>
   );
 }
