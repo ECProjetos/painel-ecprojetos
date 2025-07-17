e7b1551c887fb8ea421ac0d735ff4e46d7b8b78f
João Leonardo <joaole64@gmil.com>
2025-07-17 13:15:29 -0300

    refactor: remove unused components and simplify MarcacaoPainel

--- src/app/(private)/controle-horarios/historico/page.tsx
+++ src/app/(private)/controle-horarios/historico/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useState, useEffect } from "react";
 
 import { AlocacapPainel } from "@/components/historico/alocacao/painel-alocacao";
 import { MarcacaoPainel } from "@/components/historico/marcacao/painel-marcacao";
@@ -17,33 +16,8 @@ import {
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Clock, Folder } from "lucide-react";
 
-import { getInsightsPontoHistorico } from "@/app/actions/time-sheet/clock-controller";
-import { useUserStore } from "@/stores/userStore";
 
 export default function ControleHorariosHistoricoPage() {
-  const userId = useUserStore((state) => state.user?.id);
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const [insights, setInsights] = useState<any>(null);
-  const date = new Date();
-  const month = date.getMonth() + 1; // getMonth() retorna de 0 a 11
-  const year = date.getFullYear();
-
-  useEffect(() => {
-    async function fetchInsights() {
-      if (!userId) {
-        return;
-      }
-      try {
-        const data = await getInsightsPontoHistorico(userId as string);
-        setInsights(data);
-      } catch (error) {
-        console.error("Erro ao buscar insights:", error);
-      }
-    }
-
-    fetchInsights();
-  }, [userId]);
-
   return (
     <div className="flex flex-col bg-white shadow-lg rounded-2xl p-2 sm:p-4 sm:px-6 lg:px-8 flex-1 min-h-[125vh] border dark:bg-[#1c1c20]">
       <header className="flex h-16 shrink-0 items-center gap-2">
@@ -87,18 +61,7 @@ export default function ControleHorariosHistoricoPage() {
             </TabsTrigger>
           </TabsList>
           <TabsContent value="marcacao">
-            {insights ? (
-              <MarcacaoPainel
-                insights={insights}
-                userId={userId as string}
-                month={month}
-                year={year}
-              />
-            ) : (
-              <div className="flex items-center justify-center h-64">
-                <p className="text-gray-500">Carregando dados...</p>
-              </div>
-            )}
+            <MarcacaoPainel />
           </TabsContent>
           <TabsContent value="alocacao">
             <AlocacapPainel />
