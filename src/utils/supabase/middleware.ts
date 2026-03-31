539d074262c470875e7161dce785c54be46d8a6c
ECProjetos <ecprojetos16@gmail.com>
2026-03-31 09:22:11 -0300

    libera rota publica de satisfacao

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -15,9 +15,7 @@ export async function updateSession(request: NextRequest) {
           return request.cookies.getAll()
         },
         setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value, options }) =>
-            request.cookies.set(name, value)
-          )
+          cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value))
 
           supabaseResponse = NextResponse.next({
             request,
@@ -39,6 +37,7 @@ export async function updateSession(request: NextRequest) {
 
   const isPublicRoute =
     pathname.startsWith('/enps') ||
+    pathname.startsWith('/satisfacao') ||
     pathname.includes('/login') ||
     pathname.includes('/forgot-password') ||
     pathname.includes('/reset-password') ||
