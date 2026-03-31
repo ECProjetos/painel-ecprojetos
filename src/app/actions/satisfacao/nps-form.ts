08a42e0f5c4651119ca1f81cd1c143fd0cfc6aa5
ECProjetos <ecprojetos16@gmail.com>
2026-03-31 09:49:07 -0300

    corrige rotas publicas e formulario de satisfacao

--- src/app/actions/satisfacao/nps-form.ts
+++ src/app/actions/satisfacao/nps-form.ts
@@ -1,14 +1,19 @@
-'use server';
+'use server'
 
-import { createClient } from "@/utils/supabase/server";
+import { createClient } from "@/utils/supabase/server"
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export async function SendNpsForm(cliente: string, projeto: string, prevState: any, formData: FormData) {
-  const supabase = await createClient();
+export async function SendNpsForm(
+  cliente: string,
+  projeto: string,
+  prevState: any,
+  formData: FormData
+) {
+  const supabase = await createClient()
 
   const dataToInsert = {
-    cliente: cliente,
-    projeto: projeto,
+    cliente,
+    projeto,
     nps_score: Number(formData.get('npsScore')),
     satisfacao_geral: Number(formData.get('satisfacaoGeral')),
     aplicacao_pratica: Number(formData.get('aplicacaoPratica')),
@@ -17,30 +22,16 @@ export async function SendNpsForm(cliente: string, projeto: string, prevState: a
     comentario_geral: formData.get('comentarioGeral'),
     avanco_tecnologico: Number(formData.get('avancoTecnologico')),
     apresentacao_visual: Number(formData.get('apresentacaoVisual')),
-  };
-
-  const { error } = await supabase.from('satisfacao_clientes').insert(dataToInsert).eq('cliente', cliente).eq('projeto', projeto);
-
-  if (error) {
-    return { message: 'Erro ao enviar o formulário. Tente novamente.' };
   }
 
-  return { message: 'Formulário enviado com sucesso!' };
-}
+  const { error } = await supabase
+    .from('satisfacao_clientes')
+    .insert(dataToInsert)
 
-export async function checkExists(cliente: string, projeto: string) {
-    const supabase = await createClient();
+  if (error) {
+    console.error('Erro ao enviar formulário de satisfação:', error)
+    return { message: 'Erro ao enviar o formulário. Tente novamente.' }
+  }
 
-    const { error } = await supabase
-        .from('satisfacao_clientes')
-        .select('*')
-        .eq('cliente', cliente)
-        .eq('projeto', projeto)
-        .single();
-
-    if (error) {
-        return { exists: false };
-    }
-
-    return { exists: true };
+  return { message: 'Formulário enviado com sucesso!' }
 }
